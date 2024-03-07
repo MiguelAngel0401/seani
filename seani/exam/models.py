a847fa1df2d8604b998d7b528ba2d2da3651e3c2
Miguel Angel Islas Mendez <islasmiguel737@gmail.com>
2024-03-07 10:56:31 -0600

    05 Exam, ExamModule and Breakdown models

--- seani/exam/models.py
+++ seani/exam/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from career.models import Career
+from library.models import Module, Question
+
 # Create your models here.
 class Stage(models.Model):
+
     stage = models.IntegerField(
         verbose_name= "Etapa",
     )
@@ -25,3 +30,93 @@ class Stage(models.Model):
     class Meta:
         verbose_name = "etapa"
         verbose_name_plural = "etapas"
+
+
+class Exam(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name = "Usuario",
+        )
+    career = models.ForeignKey(
+        Career,
+        on_delete=models.CASCADE,
+        verbose_name="Carrera",
+        )
+    stage = models.ForeignKey(
+        Stage,
+        on_delete=models.CASCADE,
+        verbose_name= "Etapa"
+        )
+    modules = models.ManyToManyField(
+        Module,
+        through='ExamModule',
+        verbose_name= " Modulo",
+        )
+    questions = models.ManyToManyField(
+        Question,
+        through='Breakdown',
+        verbose_name= "Pregunta",
+        )
+    score = models.FloatField(
+        default=0.0,
+        verbose_name="Calificacion")
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name= "Fecha de Creacion",
+        )
+    updated= models.DateTimeField(
+        auto_now=True,
+        verbose_name= "Fecha de actualizacion"
+        )
+
+    def __str__(self):
+        return f"{ self.user} - { self.career} - {self.score}"
+
+    class Meta:
+        verbose_name = "examen"
+        verbose_name_plural = "examenes"
+
+class ExamModule(models.Model):
+    exam = models.ForeignKey(Exam,
+    on_delete=models.CASCADE,
+    verbose_name= "Examen"
+    )
+    module = models.ForeignKey(Module,
+    on_delete=models.CASCADE,
+    verbose_name= "Modulo"
+    )
+    active = models.BooleanField(
+        default=True,
+        verbose_name="Activo"
+        )
+    score = models.FloatField(
+        default=0.0,
+        verbose_name= "Calificacion"
+        )
+
+    def __str__(self):
+        return f"{ self.module} - { self.score}"
+
+
+class Breakdown(models.Model):
+    exam = models.ForeignKey(
+        Exam,
+        on_delete=models.CASCADE,
+        verbose_name= "Examen"
+        )
+    question = models.ForeignKey(Question,
+    on_delete=models.CASCADE,
+    verbose_name= "Pregunta"
+    )
+    answer = models.CharField(max_length=5,
+    default= '-',
+    verbose_name= "Respuesta"
+    )
+    correct = models.CharField(max_length=5,
+    default= '-',
+    verbose_name= "Respuesta Correcta"
+    )
+
+    def __str__(self):
+        return f"{ self.question} - { self.answer}"
